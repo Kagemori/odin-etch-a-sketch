0c197ed3ddc288bd010cff1205a76a252a63941d
Keith Estrella <keithestrella@gmail.com>
2025-02-15 01:40:01 -0500

    Add on hover coloring

--- javascript.js
+++ javascript.js
@@ -5,3 +5,15 @@ for (let i = 0; i < (16*16); i++){
     square.classList.add("square");
     container.append(square);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((element) => {
+    element.addEventListener("mouseenter", () =>{
+        if(element.classList.contains("colored")){
+
+        }else{
+            element.classList.add("colored");
+        }
+    })
+})
