40ab1cf875659848e34bb238c6d15113c4d607e3
Keith Estrella <keithestrella@gmail.com>
2025-02-15 01:58:26 -0500

    Add grid size adjusting

--- javascript.js
+++ javascript.js
@@ -1,19 +1,44 @@
-const container = document.querySelector("#container");
+function makeNewGrid(gridsize){
+    const container = document.querySelector("#container");
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 
-for (let i = 0; i < (16*16); i++){
-    const square = document.createElement("div")
-    square.classList.add("square");
-    container.append(square);
+    for (let i = 0; i < (gridsize * gridsize); i++){
+        const square = document.createElement("div")
+        square.classList.add("square");
+        container.append(square);
+    }
 }
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((element) => {
-    element.addEventListener("mouseenter", () =>{
-        if(element.classList.contains("colored")){
-
-        }else{
-            element.classList.add("colored");
-        }
+function squareListener(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((element) => {
+        element.addEventListener("mouseenter", () =>{
+            if(element.classList.contains("colored")){
+    
+            }else{
+                element.classList.add("colored");
+            }
+        })
     })
+}
+
+const grid = document.querySelector("#newgrid");
+
+grid.addEventListener("click", () => {
+    let size = parseInt(prompt("Enter new grid size (100 Max): "));
+
+    if(typeof(size) != "number"){
+        alert(size + " is not a number! Please try again!");
+    }else if(size > 100){
+        alert(size + " is too large!");
+    }else{
+        makeNewGrid(size);
+        squareListener();
+    }
 })
+
+// Initial - Default
+makeNewGrid(16);
+squareListener();
